29c18885564c121d6b9aa4054b9d777327b4b6aa
colincronin <colincronin@users.noreply.github.com>
2015-10-30 00:02:42 -0700

    Removed extra objects from fetchtest.php I realized that distinct method calls to the convo1 object were sufficient to achieve the desired results and then removed the extra conversation objects e.g. convo2

--- fetchtest.php
+++ fetchtest.php
@@ -9,12 +9,11 @@ $convo1->fetchAllConversations();
 
 echo '<br><br><h2>Today:</h2><h4>'.date('F j, Y',time()).'</h4>';
 
-$convo2 = new conversation();
 $today  = mktime(0, 0, 0, date("m"), date("d"), date("Y"));
 $tomorrow  = mktime(0, 0, 0, date("m"), date("d")+1, date("Y"));
 $datebegin = $today;
 $dateend = $tomorrow;
-$ids = $convo2->fetchDates($datebegin,$dateend);
+$ids = $convo1->fetchDates($datebegin,$dateend);
 print_r($ids);
 
 echo '<br><br><h2>Last 7 days:</h2>';
@@ -23,13 +22,13 @@ $lastweek  = mktime(0, 0, 0, date("m"), date("d")-7, date("Y"));
 $tomorrow  = mktime(0, 0, 0, date("m"), date("d")+1, date("Y"));
 $datebegin = $lastweek;
 $dateend = $tomorrow;
-$ids2 = $convo2->fetchDates($datebegin,$dateend);
+$ids2 = $convo1->fetchDates($datebegin,$dateend);
 print_r($ids2);
 
 echo '<br><br><h2>October 15, 2015:</h2>';
 $testday  = mktime(0, 0, 0, 10, 15, 2015);
 $testtopic = 'test';
-$convo2->addTick($testday, $testtopic);
-$ids3 = $convo2->fetchDates($testday);
+$convo1->addTick($testday, $testtopic);
+$ids3 = $convo1->fetchDates($testday);
 print_r($ids3);
 ?>
